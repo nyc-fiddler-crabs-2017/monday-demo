3698c361ad482d836c35ff0d2b1d010735086de5
Steven G. Harms <steven@devbootcamp.com>
2017-06-05 15:11:07 -0400

    Shared state demo

--- index.js
+++ index.js
@@ -3,38 +3,86 @@ class ClickCounter {
     this.elem = document.getElementById(domHook)
 
     this.props = Object.assign({
-      increment: 1,
-      clickCount: 0
+      increment: 1
     }, opts)
 
     this.state = {
-      clickCount: this.props.clickCount
     }
 
     this.elem.addEventListener("click", (e) => {
       this.incrementClickCount();
     })
-
-    this.render();
   }
 
+  // TODO :(
   incrementClickCount() {
-    this.state.clickCount += this.props.increment;
-    this.render();
+    this.props.parent.clickHandler()
   }
 
   render() {
     let body = `
       <label for="text-input">Click Count:</label>
-      <input type="text" id="text-input" value="I have been clicked ${this.state.clickCount} times"></input>
+      <input type="text" id="text-input" value="I have been clicked ${this.props.parent.state.clickCount} times"></input>
     `
     this.elem.innerHTML = body;
   }
 }
 
-new ClickCounter("click-counter-hook", {clickCount: 20, increment: 30});
-new ClickCounter("click-counter-hook2", {clickCount: 80, increment: -20});
-new ClickCounter("click-counter-hook3", {clickCount: 220});
-new ClickCounter("click-counter-hook4", {clickCount: 20});
-new ClickCounter("click-counter-hook5", {clickCount: 20});
-new ClickCounter("click-counter-hook6", {clickCount: 20});
+class OddEven {
+  constructor(domHook) {
+    this.elem = document.getElementById(domHook);
+
+    this.props = {}
+
+    this.state = {
+    }
+  }
+
+  oddEvenText() {
+    return this.props.parent.state.clickCount % 2 === 0 ? "even" : "odd"
+  }
+
+  render() {
+    let body = `<p class="${this.oddEvenText()}">${this.oddEvenText().toUpperCase()}<p>`;
+
+    this.elem.innerHTML = body;
+
+    return body;
+  }
+}
+
+class ParentWidget {
+  constructor() {
+    this.props = {
+      children: []
+    }
+
+    this.state = {
+      clickCount: 0
+    }
+  }
+
+  registerChild(c) {
+    this.props.children.push(c);
+    c.props.parent = this;
+    this.render();
+  }
+
+  clickHandler() {
+    this.state.clickCount += 1;
+
+    this.render();
+  }
+
+  render() {
+    this.props.children.forEach(c => c.render())
+  }
+}
+
+
+let cc = new ClickCounter("click-counter-hook");
+let oe = new OddEven("odd-even-hook");
+let parent = new ParentWidget();
+
+parent.registerChild(cc);
+parent.registerChild(oe);
